9bb237749bd6028221f5a6b10e55e98c8daed69a
masrafi-000 <smmasrafi000@gmail.com>
2025-03-07 15:30:53 +0600

    Updated

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex flex-col items-start justify-start min-h-svh">
+      <Button className="bg-red-600" >Click me</Button>
+    </div>
+  );
+}
+
+export default App;
